fb675b9181f276364bce233d4f2266225de5bf2c
Mide <mideojacobs@yahoo.com>
2023-07-18 21:44:19 -0600

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	} else if (n == 0)
+	} 
+	else if (n < 0)
 	{
-		-putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
-	}
+		-putchar(45);
 		return (-1);
+	} 
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+		putchar('\n');
 }
 
